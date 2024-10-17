19f736df7e1e5839868ec4b37b9ebdb8abbedaf2
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-10-17 15:39:18 +0530

    Connect to DB

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
-
 // connectDB
+const connectDB = require("./db/connect");
 
 // routes Imports
 const jobsRouter = require("./routes/jobs");
@@ -27,6 +27,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
+
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
