fce052b69d2b46e4964e117aa8b089f9cd698b39
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-07 21:51:10 +0530

    Update Error Handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,13 +6,23 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something Went OnZY MonZY Please try again Later",
   };
 
+  // Incomplete Input
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
+  }
+
+  //Duplicate Val
   if (err.code && err.code === 11000) {
-    customError.msg = `Duplicate Value Entered for ${Objec.keys(
+    customError.msg = `Duplicate Value Entered for ${Object.keys(
       err.keyValue
     )} Field , Please choose another value`;
 
     customError.statusCode = 400;
   }
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
