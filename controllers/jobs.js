5b06af027bcdb691f7249fb6df6be36d308df967
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-07 20:24:05 +0530

    Setup Get SIngle Job

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -9,7 +9,20 @@ const getAllJobs = async (req, res) => {
 };
 
 const getJob = async (req, res) => {
-  res.send("Get a single Job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No Job With That Id) ${jobId} `);
+  }
+  res.status(StatusCodes.OK).json(job);
 };
 
 const createJob = async (req, res) => {
