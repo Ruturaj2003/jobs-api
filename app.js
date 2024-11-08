90761dfb1a3ce4faaea989a270798008a45056b2
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-11-08 20:04:44 +0530

    Add Limiter Again And Finish Project

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
-// app.use(
-//   rateLimiter({
-//     windowMs: 15 * 60 * 1000, // 15 minutes
-//     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-//   })
-// );
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
